4020c4ac9d1129631e4268a38b630da1ac81ad73
MavDavis <ekwesimarvelous@gmail.com>
2022-08-12 19:53:44 +0100

    commit

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,6 +8,7 @@
 export default {
 
   mounted(){
+    fetch("https://fakestoreapi.com").then(res => res.json()).then(data => console.log(data))
     let userinfo = localStorage.getItem("user-info")
     if (userinfo){
        return
