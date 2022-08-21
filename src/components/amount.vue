7f802f23a835884ecc6b3cb8d1dfcd7743f35182
Mavdavis <ekwesimarvelous@gmail.com>
2022-08-21 16:14:00 -0700

    commit

---
+++ src/components/amount.vue
@@ -0,0 +1,28 @@
+<template>
+<h1>HelloWorld</h1>
+<input type="number"  value="{{sumItem}}"/>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+export default {
+    name:"Amount",
+    props:["amount"],
+  setup() {
+
+  },
+  computed:{
+    sumItem(){
+    console.log(this.localItems);
+      var sum = this.localItems.reduce((acc, item) => acc + item.price, 0);
+return(sum);
+      
+    }
+  },
+  created(){
+  }
+};
+</script>
+
+<style>
+</style>
