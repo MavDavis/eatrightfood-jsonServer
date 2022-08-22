95b1b8f942b3383d12457ec0fe0d14c869f77290
Mavdavis <ekwesimarvelous@gmail.com>
2022-08-21 22:05:58 -0700

    commit

--- src/components/amount.vue
+++ src/components/amount.vue
@@ -1,6 +1,9 @@
 <template>
-<h1>HelloWorld</h1>
-<input type="number"  value="{{sumItem}}"/>
+<div v-for="data in datas" :key="data.id">
+<h4>{{data.title.slice(0,15)}}..</h4>
+<p>${{data.price}}</p>
+<p>{{data.quantity}}</p>
+</div>
 </template>
 
 <script>
@@ -8,6 +11,12 @@ import { ref } from '@vue/reactivity';
 export default {
     name:"Amount",
     props:["amount"],
+    data(){
+      return{
+        datas:[],
+        
+      }
+    },
   setup() {
 
   },
@@ -20,6 +29,8 @@ return(sum);
     }
   },
   created(){
+        const localItems = ref(JSON.parse(localStorage.getItem("cart")));
+this.datas = localItems
   }
 };
 </script>
--- src/pages/payment.vue
+++ src/pages/payment.vue
@@ -1,4 +1,5 @@
 <template>
+<div  class="paystackvue">
   <section>
     <form class="paymentForm" @submit.prevent="payWithPaystack">
       <div class="form-group" @click="show" @mouseleave="hide">
@@ -22,7 +23,11 @@
       </div>
     </form>
   </section>
-  <Amount :amount="amount" />
+<section>
+    <Amount :amount="amount" />
+
+</section>
+</div>
 </template>
 
 <script>
@@ -87,10 +92,27 @@ export default {
 };
 </script>
 
-<style scoped>
-section {
+<style scoped lang="scss">
+.paystackvue{
   position: relative;
-  top: 5rem;
-  height: 100vh;
+  top:5rem;
+  padding: 0 5%;
+  margin: 10rem 0;
+  display: flex;
+  flex-wrap: wrap;
+  width:100%;
+  min-height: 100vh;
+  justify-content: space-between;
+
+  section{
+    flex-basis: 45%;
+
+   
+  }
+   @media(max-width: 600px){
+flex-direction: column-reverse;
+
+    }
 }
+
 </style>
